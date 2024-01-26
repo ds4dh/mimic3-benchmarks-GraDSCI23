1e4e4b2b5c01699db164a511676181cb71e49573
Dimitrios Proios <dimi.proios@gmail.com>
2024-01-26 16:27:00 +0100

    use reuseable code for m2 and m3 connectivity strategies

--- gnn__models/connectivity_strategies/expert_graph_m2_inter_category.py
+++ gnn__models/connectivity_strategies/expert_graph_m2_inter_category.py
@@ -12,6 +12,7 @@ import pandas as pd
 
 import sys
 from gnn__models.config.config import merge_features, orig_rules, rules, rules_normal, all_mappings 
+from gnn__models.connectivity_strategies.common_code_for_strategies import create_category_files
 
 FOLDER_STRUCTURE = 'data/phenotyping/'
 
@@ -43,142 +44,7 @@ if clean:
     os.system(f'rm A_expert_edges_exact{spstr}.pk')
 
 
-fname = f'dict_dfs_cat.pk{spstr}'
-if not os.path.exists(fname):
-    # 1m 
-    # parse all files in the folder path recursively
-    def parse_folder(path):
-        count = 0
-        
-        for root, dirs, files in os.walk(path):
-            for file in files:
-                count += 1 
-                if count % 10000 == 0:
-                    print(count)
-                if count > 60000:
-                    raise Exception('too many something is wrong ')
-                if 'episode' in file:
-                    yield os.path.join(root, file)
-
-    filenames = []
-    dict_dfs = {}
-
-    try:
-        for i, file in enumerate(parse_folder(FOLDER_STRUCTURE)):
-            filenames.append(file)
-            df= pd.read_csv(file)
-            dict_dfs[file] = df 
-            if small_part and i > 100:
-                break
-            # if i > 40:
-            #     break
-    except Exception as e:
-        print(e)
-        pass
-
-    print(' Conversion of categorical values')
-
-
-    def inverse_mapping(mapping):
-        inverse = {}
-        for key in mapping:
-            for value in mapping[key]:
-                inverse[value] = key
-        return inverse
-    all_mappings_inv={col:inverse_mapping(all_mappings[col]) for col in all_mappings.keys()}
-    all_mappings_inv
-
-    #### 2m
-    print('convert_values_of_categorical_columns')
-    def convert_values_of_categorical_columns(df):
-        # replace the wrong values with the correct ones
-        df.replace(all_mappings_inv, inplace=True)
-        categorical_columns = [col for col in df.columns if df[col].dtype == 'object']
-        for col in categorical_columns:
-            #get from 1 XXX to -> 1
-            df[col] = df[col].str.split(" ").apply(
-                lambda x: x[0] if isinstance(x, list) else x)
-            # convert categorical column to numeric
-            df[col] = pd.to_numeric(df[col], errors='coerce')
-        return df
-        
-
-    dict_dfs_processed = {}
-    for k, df in dict_dfs.items():
-
-        res = convert_values_of_categorical_columns(df.copy())
-        if res is None:
-            print(k)
-            break
-        dict_dfs_processed[k]= res
-
-
-    # dict_dfs_processed = pickle.load(open('dict_dfs_processed.pk', 'rb'))
-
-    print(' 3m create categories per column & df')
-    dict_dfs_cat = {}
-    for k, df in tqdm(dict_dfs_processed.items()):
-        df = df.copy()
-        df.drop('Hours', axis=1, inplace=True)#.shape
-
-        if (df['Height'] is not None) and (df['Weight'] is not None):   
-            # front fill the column Heigth
-            df['Height'].fillna(method='ffill', inplace=True)
-            # if there is nan in column height back fill it
-            df['Height'].fillna(method='bfill', inplace=True)
-            
-            df['BMI']=(df['Weight']/((df['Height']/100)**2))
-        else:
-            df['BMI']=math.nan
-        df.drop(['Height', 'Weight', 'Fraction inspired oxygen'], axis=1, inplace=True)
-        
-        for col in rules.keys():
-                
-            ### apply list of lambda and return the index of the first true value
-            arr1 = df[col].apply(lambda x: [rule(x) for rule in rules[col]])
-            arr = arr1.apply(lambda x: x.index(True) if True in x else None)
-            df[col]=arr
-
-        dict_dfs_cat[k] = df.apply(lambda x:x.unique())
-        pickle.dump(dict_dfs_cat, open('dict_dfs_cat.pk', 'wb'))
-        # if all the values are nan then add normal category
-        # dict_dfs_cat[k].fillna(rules_normal, inplace=True)
-
-counter +=1 
-print(f'Phase {counter}')
-fname= f'alldfs_categories{spstr}.csv'
-
-if not os.path.exists(fname):
-    dict_dfs_cat = pickle.load(open('dict_dfs_cat.pk', 'rb'))
-    print('### remove nans and Nones')
-    dict_dfs_cat_uniform = {}
-    for k, series in tqdm(dict_dfs_cat.items()):
-        
-        series = series.apply(lambda x: sorted(x))
-        # filter nan values from lists in series cells
-        series = series.apply(lambda x: [i for i in x if i is not None])
-        try:
-            if len(series) > 1:
-                # series = series.append(pd.Series([math.nan for i in range(len(series.columns))]), ignore_index=True)
-                series = series.apply(lambda x: [i for i in x if not math.isnan(i)])
-                
-        except:
-            import pdb; pdb.set_trace() 
-            # /home/dproios/work/create_EHR_gra/mimic3-benchmarks/data/phenotyping/train/72456_episode1_timeseries.csv
-        dict_dfs_cat_uniform[k] = series
-    try:
-
-        # convert to pd.DataFrame a dict with series values 
-        alldfs = pd.DataFrame.from_dict(dict_dfs_cat_uniform, orient='index',columns = (list(dict_dfs_cat_uniform.values())[0]).index.tolist())
-        
-        # alldfs = pd.DataFrame(dict_dfs_cat_uniform)
-        # # alldfs=pd.concat(list(dict_dfs_cat_uniform.values()),axis=1).T
-        # # alldfs.index=list(dict_dfs_cat_uniform.keys())
-        alldfs.to_csv(fname)
-        
-    except:
-        import pdb; pdb.set_trace() 
-
+create_category_files(FOLDER_STRUCTURE, spstr)
 import itertools
 def apply_logical_and_axis_1_to_df(df):
     df = df.copy()
